4a756efd9c8ec8aaf78cf81fed17158848df43fe
monica <monica@IP-C6136007.corp.amazonworkspaces.com>
2016-11-21 07:31:41 -0800

    test 1

---
+++ src/test/java/firstTestSelenium/NewTest.java
@@ -0,0 +1,28 @@
+package firstTestSelenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	public String exePath = "D:\\Users\\monica\\Downloads\\chromedriver_win32\\chromedriver.exe";
+	public String url = "https://google.com";
+	// System.setProperty("webdriver.firefox.marionette", exePath);
+	// WebDriver driver = new FirefoxDriver();
+	public String urlChrome = System.setProperty("webdriver.chrome.driver", exePath);
+	public WebDriver driver = new ChromeDriver();
+  @Test
+  public void f() {
+	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		// Launch website
+		driver.navigate().to(url);
+		System.out.println("Application title is =============" + driver.getTitle());
+		// Maximize the browser
+		driver.manage().window().maximize();
+		System.out.println("success test okeeee!!!"+url);
+		System.out.println("success test okeeee!!!"+url);
+	  
+  }
+}

3a52f616d03c1f15d197a1290c753f8ec2cf39cc
monicadanesa <monicadanesa@gmail.com>
2016-11-24 07:43:13 -0800

    a

--- src/test/java/searchGoogle/searchPage.java
+++ src/test/java/searchGoogle/searchPage.java
@@ -20,7 +20,6 @@ public class searchPage {
 	public void clickButton() {
 		driver.findElement(button).click();
 	}
-	
 
 	public void sendKeyWordGoogle(String KeyWord) {
 		this.setKeyWord(KeyWord);

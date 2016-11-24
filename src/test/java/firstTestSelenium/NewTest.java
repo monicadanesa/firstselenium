2ea469b242cdf5bd65c276ba1a2b6ab4eabf662c
monicadanesa <monicadanesa@gmail.com>
2016-11-24 09:29:00 -0800

    rvert to 85c7d7f

---
+++ src/test/java/firstTestSelenium/NewTest.java
@@ -0,0 +1,75 @@
+package firstTestSelenium;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.*;
+
+public class NewTest {
+	
+	public String exePath;
+	public String setDriver;
+	// System.setProperty("webdriver.firefox.marionette", exePath);
+	// WebDriver driver = new FirefoxDriver();
+	WebDriver driver;
+
+
+  @Parameters ({"BROWSER"})
+  @BeforeMethod
+  public void testBrowser(String BROWSER)throws Exception{
+	 
+	 if(BROWSER.equalsIgnoreCase("FF"))
+	  {	exePath = "D:\\Users\\monica\\Downloads\\geckodriver-v0.11.1-win64\\geckodriver.exe";
+		 //System.setProperty("webdriver.firefox.bin", exePath);
+		 System.setProperty("webdriver.firefox.marionette",exePath);        
+		 driver = new FirefoxDriver();
+		 System.out.println("FF");
+	  }
+	  else if (BROWSER.equalsIgnoreCase("CH"))
+	  {
+		  exePath = "D:\\Users\\monica\\Downloads\\chromedriver_win32\\chromedriver.exe";
+		  setDriver = System.setProperty("webdriver.chrome.driver", exePath);
+		  driver = new ChromeDriver();
+		  System.out.println("CH");
+	  }
+	 
+	 
+	else if(BROWSER.equalsIgnoreCase("IE")){
+
+          //set path to IEdriver.exe You may need to download it from
+
+   // 32 bits http://selenium-release.storage.googleapis.com/2.42/IEDriverServer_Win32_2.42.0.zip
+
+   // 64 bits http://selenium-release.storage.googleapis.com/2.42/IEDriverServer_x64_2.42.0.zip
+
+          System.setProperty("webdriver.ie.driver","D:\\Users\\monica\\Downloads\\IEDriverServer_x64_2.42.0\\IEdriver.exe");
+     
+          //create chrome instance
+
+         driver = new InternetExplorerDriver();
+		System.out.println("IE");
+      }
+	  
+	  else{
+		  throw new Exception("Browser is not correct");
+	  }
+	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	  driver.get("https://google.com");
+  }
+  
+  
+	
+  @Test
+  public void f()  throws InterruptedException {
+	 
+		// Launch website
+		System.out.println("Application title is =============" + driver.getTitle());
+		// Maximize the browser
+		driver.manage().window().maximize();
+		System.out.println("success test okeeee!!!");
+	  
+  }
+
+}
--- src/test/java/searchGoogle/searchTest.java
+++ src/test/java/searchGoogle/searchTest.java
@@ -4,42 +4,27 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import searchGoogle.searchPage;
+import searchGoogle.BasePage;
 
-
-public class searchTest{
+public class searchTest extends BasePage{
 	WebDriver driver;
 	searchPage objPage;
-	String exePath;
-	
+
 	@Parameters({"BROWSER"})
 	@BeforeTest
 	public void setUp(String BROWSER) {
-
-		 if (BROWSER.equalsIgnoreCase("CH"))
-		  {
-			  exePath = "D:\\Users\\monica\\Downloads\\chromedriver_win32_latest\\chromedriver.exe";
-			  //exePath ="/usr/bin/chromedriver";
-			  System.setProperty("webdriver.chrome.driver", exePath);
-			  driver = new ChromeDriver();
-			  System.out.println("CH");
-			  
-		  }
-		 
-	
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("http://google.com");
-		driver.manage().window().maximize();
+		BasePage bp = new BasePage();
+		bp.setUp(BROWSER);
 		
 	}
 
 	@Test
 	public void f() {
 		objPage = new searchPage(driver);
-		objPage.sendKeyWordGoogle("applee");
+		objPage.sendKeyWordGoogle("apple");
 	}
 }

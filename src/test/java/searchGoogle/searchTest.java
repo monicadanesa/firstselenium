4c1ab34f9869624b2f63597be13ed8980fa31e80
monicadanesa <monicadanesa@gmail.com>
2016-11-24 07:09:22 -0800

    a

--- src/test/java/searchGoogle/searchTest.java
+++ src/test/java/searchGoogle/searchTest.java
@@ -23,8 +23,8 @@ public class searchTest{
 		 if (BROWSER.equalsIgnoreCase("CH"))
 		  {
 			  //exePath = "D:\\Users\\monica\\Downloads\\chromedriver_win32\\chromedriver.exe";
-			  exePath ="/usr/bin/chromedriver";
-			  System.setProperty("webdriver.chrome.driver", exePath);
+			  //exePath ="/usr/bin/chromedriver";
+			  //System.setProperty("webdriver.chrome.driver", exePath);
 			  driver = new ChromeDriver();
 			  System.out.println("CH");
 			  
@@ -33,9 +33,9 @@ public class searchTest{
 		 else if(BROWSER.equalsIgnoreCase("FF"))
 			  {	
 			 	//exePath = "D:\\Users\\monica\\Downloads\\geckodriver-v0.11.1-win64\\geckodriver.exe";
-			 	exePath ="/usr/bin/chromedriver/geckodriver.exe";
+			 	//exePath ="/usr/bin/chromedriver/geckodriver";
 				 //System.setProperty("webdriver.firefox.bin", exePath);
-				 System.setProperty("webdriver.firefox.marionette",exePath);        
+				 //System.setProperty("webdriver.firefox.marionette",exePath);        
 				 driver = new FirefoxDriver();
 				 System.out.println("FF");
 			  }
